94294a0c173c7e73838e216580d7b696562e440f
jtamaki <junji.tamaki@exmotion.co.jp>
2023-12-21 20:59:39 +0900

    Update run_headless.sh (#16)

--- simulator/run_headless.sh
+++ simulator/run_headless.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo nvidia-xconfig --preserve-busid -a --virtual=1280x1024
+BUS_ID=$(nvidia-xconfig --query-gpu-info | grep 'PCI BusID' | sed -r 's/\s*PCI BusID : PCI:(.*)/\1/')
+sudo nvidia-xconfig -a --virtual=1280x960 --allow-empty-initial-configuration --enable-all-gpus --busid $BUS_ID
 sudo Xorg :0 &
 sleep 1
 ./bin/LinetraceWorld/LinetraceWorld.sh -env=/tmp/simulator_env.json -RenderOffscreen
